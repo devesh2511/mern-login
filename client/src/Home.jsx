d8f16371ce9211aac6a256f9ca8f5a4c99f71aca
deveshjain2511 <76024137+devesh2511@users.noreply.github.com>
2023-07-19 22:05:01 +0530

    removed jwt

---
+++ client/src/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Login Successful</h1>
+      <div className="home-image">
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/UserNotFound.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+const UserNotFound = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="contianer-02"> 
+      <div className="img-box">
+      </div>
+      <div className="text-box">
+        <h1>User Not Found!</h1>
+        <div className="btn-group">
+        <button className='login' onClick={()=>{navigate('/login')}}>Try Again</button>
+        <button className='register' onClick={()=>{navigate('/register')}}>New Register</button>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default UserNotFound
